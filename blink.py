6242341e2393889199042f779937c5441467b391
bellachip49 <krystal.m.kwan@gmail.com>
2015-05-30 16:40:59 -0700

    blink LED on GPIO17/pin 11

--- blink.py
+++ blink.py
@@ -1,30 +1,17 @@
 import RPi.GPIO as GPIO  
 import time  
 # blinking function  
-def blink(pin,pin2):  
+def blink(pin):  
         GPIO.output(pin,GPIO.HIGH)  
        	time.sleep(1)
-	GPIO.output(pin2,GPIO.HIGH)
-	time.sleep(1)  
-        GPIO.output(pin,GPIO.LOW)  
-        time.sleep(1)
-	GPIO.output(pin2,GPIO.LOW)
 	time.sleep(1)  
         return  
-def blink2(pin):
-	GPIO.output(pin,GPIO.HIGH)
-	time.sleep(1)
-	GPIO.output(pin,GPIO.LOW)
-	time.sleep(1)
-	return
 # to use Raspberry Pi board pin numbers  
 GPIO.setmode(GPIO.BOARD)  
 # set up GPIO output channel  
-GPIO.setup(15, GPIO.OUT)  
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)  
 # blink GPIO17 50 times 
 for i in range(0,50): 
-	blink(15,16)
-	#blink2(16)
+	blink(11)
 GPIO.cleanup()  
  
